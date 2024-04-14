fb0228f829d0e87ccafeaf494713733a643feed5
bnithin06 <bnithin2822@gmail.com>
2024-04-14 11:50:36 +0530

    CourseDetailsPage updated

--- lms-frontend/src/components/Courses/CourseDetailsPage.jsx
+++ lms-frontend/src/components/Courses/CourseDetailsPage.jsx
@@ -80,12 +80,34 @@ function CourseDetailsPage() {
                 <div className='flex flex-col md:flex-row m-4'>
                     <div className=' w-1/2 '>
                        <p>{course.description}</p> 
+                       <div>
+                       <div className="flex items-center mb-4 mt-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-600 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M2 0h16a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm8 15a1 1 0 0 0 1-1v-1a1 1 0 1 0-2 0v1a1 1 0 0 0 1 1zm-3-3a1 1 0 0 0 1-1V7a1 1 0 0 0-2 0v4a1 1 0 0 0 1 1zm6 0a1 1 0 0 0 1-1V7a1 1 0 0 0-2 0v4a1 1 0 0 0 1 1z" clipRule="evenodd"/>
+                            </svg>
+                            <p className="text-gray-600">Beginner Level</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-600 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-2a6 6 0 1 0 0-12 6 6 0 0 0 0 12zm-5-3a1 1 0 0 0 1.447.895l3.789-1.895a1 1 0 0 0 .342-1.74l-3.789-2.263A1 1 0 0 0 5 9V5a1 1 0 1 0-2 0v4a3 3 0 0 0 1.158 2.37l3.789 2.263a1 1 0 0 0 .342 1.74l-3.789 1.895A1 1 0 0 0 5 15z" clipRule="evenodd"/>
+                            </svg>
+                            <p className="text-gray-600">Duration: 10 weeks</p>
+                         </div>
+                         <div>
+                            <button onClick={handleEnroll} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Enroll Now</button>
+                        </div>
+                       </div>
                     </div>
                     <div className='flex items-start justify-center w-1/2'>
                         <div className="w-fit h-fit bg-white p-3">
                             <iframe className="w-full h-full rounded-lg" src={`https://www.youtube.com/embed/${getVideoId(course.video_url)}`} frameBorder="0" allowFullScreen></iframe>
                         </div>
                     </div>
+                </div>
+
+
+                <div>
+                    <h1 className='text-center text-xl font-semibold'>Course Features</h1>
                 </div>
                 </>
             ) : (
